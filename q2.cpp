2a696770858a0bf9edd0656455d89dd6d508c389
KISHAN SADHU <kishansadhusadhu69@gmail.com>
2025-07-13 18:39:59 +0530

    done_13/7/25

---
+++ q2.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*
+    //pattenrs
+
+    1).triangle
+    *
+    **
+    ***
+    ****
+    --- code ---
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    2).apposite side triangle
+     *
+    **
+   ***
+  ****
+  --- code ---
+  for(int i=1;i<=4;i++){
+        for(int j=4;j>=i;j--){
+            cout << " ";
+        }
+        for(int k=1;k<=i;k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    3).inverted triangle
+    ****
+    ***
+    **
+    *
+    --- code ---
+      for(int i=1;i<=4;i++){
+        for(int j=1;j<=5-i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    4).inverted apposite side triangle
+     ****
+      ***
+       **
+        *
+    --- code ---
+    for(int i=4;i>=1;i--){
+        for(int j=4;j>=i;j--){
+            cout << " ";
+        }
+        for(int k=1;k<=i;k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+    5).pyramid
+       *
+      ***
+     *****
+    --- code ---
+    for(int i=1;i<=4;i++){
+        for(int j=4;j>=i;j--){
+            cout << " ";
+        }
+        for(int k=1;k<=i;k++){
+            cout << "*";
+        }
+        for(int k=2;k<=i;k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    6).inverted pyramid
+     *******
+      *****
+       ***
+        *
+    --- code ---
+     for(int i=4;i>=1;i--){
+        for(int j=4;j>=i;j--){
+            cout << " ";
+        }
+        for(int k=1;k<=i;k++){
+            cout << "*";
+        }
+        for(int k=2;k<=i;k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    */
+    //next is harder then above , i will be implement latter
+    return 0;
+}
